a764a2310bb2b49db3e87006ec81b996bfcc4b4d
darceno <123845571+darceno@users.noreply.github.com>
2023-04-06 20:47:16 -0300

    Change 'add' function input description for clarification

--- terminal_expenses.py
+++ terminal_expenses.py
@@ -1,7 +1,7 @@
 def add():
     """ This function asks for a value and, if it's a number, adds it to the expenses.txt file """
     with open('data/expenses.txt', 'a') as f:
-        new_expense = input("New expense: ")
+        new_expense = input("New expense (type 0 when finished): ")
         while new_expense != "0":
             if new_expense.isdigit():
                 f.write(new_expense + "\n")
